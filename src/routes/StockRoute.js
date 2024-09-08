b56fa92c5c61ad84150fad0584657eb6e996c82b
Valdeir de Souza Prestes <valdeirprestes@gmail.com>
2024-09-08 02:56:05 -0300

    Ajuste da contagem de estoque

--- src/routes/StockRoute.js
+++ src/routes/StockRoute.js
@@ -8,6 +8,6 @@ route.post("/todos",tokenrequiremiddleware, stockcontroller.getall);
 route.post("/adicionar", tokenrequiremiddleware, stockcontroller.add);
 route.post("/remover", tokenrequiremiddleware, stockcontroller.sub);
 
-route.get("/quantidade", tokenrequiremiddleware, stockcontroller.count);
-route.get("/saldo", tokenrequiremiddleware, stockcontroller.saldo);
+route.post("/quantidade", tokenrequiremiddleware, stockcontroller.count);
+route.post("/saldo", tokenrequiremiddleware, stockcontroller.saldo);
 export default route;
